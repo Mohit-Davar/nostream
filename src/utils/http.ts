e2ee80e13c06acfb8312ade04a4319568bf2c2c5
Mohit-Davar <mohitdavar2004@gmail.com>
2026-04-10 01:01:55 +0530

    TODO : deprication removal

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -3,15 +3,7 @@ import { IncomingMessage } from 'http'
 import { Settings } from '../@types/settings'
 
 export const getRemoteAddress = (request: IncomingMessage, settings: Settings): string => {
-  let header: string | undefined
-  // TODO: Remove deprecation warning
-  if ('network' in settings && 'remote_ip_header' in settings.network) {
-    console.warn(`WARNING: Setting network.remote_ip_header is deprecated and will be removed in a future version.
-        Use network.remoteIpHeader instead.`)
-    header = settings.network['remote_ip_header'] as string
-  } else {
-    header = settings.network.remoteIpHeader as string
-  }
+  const header = settings.network.remoteIpHeader
 
   const result = (request.headers[header] ?? request.socket.remoteAddress) as string
 
